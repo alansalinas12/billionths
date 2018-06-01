1d1b5ad219c1d3b69e6bcf990e8c3bfa087cef7e
Alan Salinas <alanrene19@gmail.com>
2018-06-01 12:11:13 -0500

    merge

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+var passport = require('passport');
+var session = require('express-session');
+var exphbs = require('express-handlebars');
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'keyboard cat',
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+        httpOnly: false
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// setup rendering engine
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+app.set('views', path.join(__dirname, 'views'));
+
+require('./routes/htmlRoutes')(app);
+require('./routes/transactions')(app);
+require('./routes/users')(app, passport);
+require('./config/passport')(passport, db.User);
+
+require('dotenv').config();
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+});
+
+module.exports = {app: app, db: db};
