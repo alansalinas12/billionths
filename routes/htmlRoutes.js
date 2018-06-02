05cc3b81c86af016e42401d19bf4cb739923f53d
Alan Salinas <alanrene19@gmail.com>
2018-06-02 11:05:53 -0500

    test

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,34 +16,18 @@ module.exports = function(app){
 
 	app.get("/dashboard", (req, res) => {
 		res.render("dashboard");
+    })
 
-		app.get("/contact", (req, res) => {
-			res.render("contact");
-		})
-
-		app.get("/about", (req, res) => {
-			res.render("about");
-		})
-
-		app.get("/transactions", (req, res) => {
-			res.render("transactions");
-		})
+	app.get("/contact", (req, res) => {
+		res.render("contact");
 	})
 
-	app.get("/profile/:id", (req, res) => {
-		db.User.findOne({
-			where: {
-				id: req.params.id
-			},
-			include: [{
-				model: db.Transaction,				
-			}]
-		}).then(function (dbUser) {
-			res.render("profile", {
-				user: dbUser
-			});
-		}).catch((err) => {
-			res.json(err);
-		});
-	});
+	app.get("/about", (req, res) => {
+		res.render("about");
+	})
+
+	app.get("/transactions", (req, res) => {
+		res.render("transactions");
+	})
+	
 }
